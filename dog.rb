175108a66ebfc8482a8996e13e50d6cab6c7c2cb
apw601 <apw601@gmail.com>
2018-06-11 16:14:12 +0000

    automatically backed up by learn

--- dog.rb
+++ dog.rb
@@ -1,11 +1,11 @@
 class Dog
-  def name(dog_name)
+  def name=(dog_name)
     @name = dog_name
   end
   def name
     @name
   end
-  def breed(dog_breed)
+  def breed=(dog_breed)
     @breed = dog_breed
   end
   def breed
