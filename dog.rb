ff615626018fd2d5065082e8c76bda51ec871bc8
apw601 <apw601@gmail.com>
2018-06-11 16:13:16 +0000

    automatically backed up by learn

--- dog.rb
+++ dog.rb
@@ -2,10 +2,21 @@ class Dog
   def name(dog_name)
     @name = dog_name
   end
+  def name
+    @name
+  end
   def breed(dog_breed)
     @breed = dog_breed
   end
+  def breed
+    @breed
+  end
   def bark
-    puts "bark"
+    puts "Woof!!!"
   end
 end
+
+Dog.new = arthur
+puts arthur.name = "Arthur"
+puts athur.breed = "cockapoo mix"
+puts arthur.bark
